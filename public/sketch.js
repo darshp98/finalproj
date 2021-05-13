2cf88302deaa2f8d9fff1fe947f6dd1e0eca24f8
Darshana <dp2556@nyu.edu>
2021-05-13 14:55:19 -0400

    post meeting fixes

--- public/sketch.js
+++ public/sketch.js
@@ -1,27 +1,22 @@
-var socket;
-var captionInput, userGuess, submitButton, nextButton;
+//to do:
+//timer
+//maybe include player num of each client
+
 //drawing
 var buttonRed, buttonBlue, buttonGreen, buttonOrange, buttonYellow, buttonViolet, buttonWhite, buttonBlack, buttonClear;
 var redVal = 255, greenVal = 0, blueVal = 0;
 var strokeSlider;
 //
+var socket;
+var captionInput, userGuess, submitButton, nextButton;
 var firstinstruction, firstphrase;
+var timer = 30;
 var phrases = ['dog', 'cat', 'mouse', 'santa', 'snowman', 'elf', 'alien', 'spaceship', 'rocket'];
 var index = 0;
 var roundinfo = {
   newIndex: index,
   newPhrase: phrases[index]
-}
-var clientPoints = {};
-var  timer =60;
-
-//NEXT ROUND STUFF TO FIX:
-//increase index by one for every client not just client that clicked
-//figure out how to clear canvas for every client when clicked
-//OTHERS:
-//timer
-//maybe include player num of each client
-//deploy to heroku
+} //sends the player index and player phrase
 
 function setup() {
   createCanvas(500, 500);
@@ -29,7 +24,9 @@ function setup() {
   socket = io.connect('http://localhost:3000');
   socket.emit('turn', roundinfo);
   socket.on('turn', firstRound);
+  socket.on('update', updated);
   socket.on('drawing', showDrawing);
+  socket.on('clear', makeClear);
 
   socket.on('disconnect', function () {
     socket.disconnect();
@@ -59,14 +56,14 @@ function setup() {
   buttonClear = select('#Clear');
 
   buttonRed.position(400, 600);
-  buttonOrange.position(460, 600);
-  buttonYellow.position(520, 600);
-  buttonGreen.position(580, 600);
-  buttonBlue.position(640, 600);
-  buttonViolet.position(700, 600);
-  buttonWhite.position(820, 600);
-  buttonBlack.position(760, 600);
-  buttonClear.position(950, 600);
+  buttonOrange.position(455, 600);
+  buttonYellow.position(510, 600);
+  buttonGreen.position(565, 600);
+  buttonBlue.position(620, 600);
+  buttonViolet.position(675, 600);
+  buttonWhite.position(730, 600);
+  buttonBlack.position(785, 600);
+  buttonClear.position(840, 600);
 
   buttonRed.mousePressed(makeRed);
   buttonOrange.mousePressed(makeOrange);
@@ -82,20 +79,26 @@ function setup() {
   strokeSlider.position(400, 650)
 }
 
-function firstRound(currPhrase) {
+function firstRound(broadcasted) { 
   nextButton.show();
-  firstinstruction = createP("Draw this: " + currPhrase);
+  firstinstruction = createP("Draw this: " + broadcasted.pphrase);
   firstinstruction.position(600, 10)
 }
 
-function nextRound() { //only occurs for person who clicks button- how to update for everyone?
+function updated(broadcasted) { //recieves increased index values from  prev player and sent to everyone
+  roundinfo.newIndex = broadcasted.newIndex; //second player info = first player updated info
+  roundinfo.newPhrase = broadcasted.newPhrase;//puts new values into array
+  console.log(roundinfo)
+}
+
+function nextRound() { //once clicked next button
+  roundinfo.newIndex += 1; //increases index
+  roundinfo.newPhrase = phrases[roundinfo.newIndex] //increases phrase index
   firstinstruction.hide();
   nextButton.hide();
   background('white');
-  roundinfo.newIndex += 1;
-  roundinfo.newPhrase = phrases[roundinfo.newIndex]
-  socket.emit('turn', roundinfo);  
-  console.log(roundinfo)
+  socket.emit('turn', roundinfo); //sends to server
+  socket.emit('update', roundinfo); //sends to server
 }
 
 let Py = 100;
@@ -115,21 +118,20 @@ function enteredGuess() {
 function draw() {
   textAlign(LEFT);
   clock = select('#circle');
-  clock.position(400,10);
+  clock.position(400, 10);
   time = createP(timer);
-  time.position(410,10)
-  if (frameCount%60 ==0 && timer> 0) {
+  time.position(410, 10)
+  if (frameCount % 60 == 0 && timer > 0) {
     timer--;
   } else if (timer == 0) {
-    text("Times Up! The word was: " + roundinfo.newPhrase, 150,250)
+    text("Times Up! The word was: " + roundinfo.newPhrase, 150, 250)
   }
 }
 
 function showDrawing(points) {
-  clientPoints = points;
-  stroke(clientPoints.r, clientPoints.g, clientPoints.b);
-  strokeWeight(clientPoints.size);
-  line(clientPoints.x, clientPoints.y, clientPoints.x + 1, clientPoints.y + 1)
+  stroke(points.r, points.g, points.b);
+  strokeWeight(points.size);
+  line(points.x, points.y, points.x + 1, points.y + 1)
 }
 
 //canvas drawing functions
@@ -180,8 +182,10 @@ function makeBlack() {
   greenVal = 0;
   blueVal = 0;
 }
+
 function makeClear() {
   background('white');
+  socket.emit('clear');
 }
 
 function mouseDragged() {
--- server.js
+++ server.js
@@ -4,29 +4,45 @@ var express = require('express');
 var app = express();
 app.use(express.static('public'));
 
-//var server = app.listen(3000);
+var server = app.listen(3000);
 //heroku
-var port = process.env.PORT || 3000;
-var server = app.listen(port);
+// var port = process.env.PORT || 3000;
+// var server = app.listen(port);
 
 var socket = require('socket.io');
 var io = socket(server);
 io.sockets.on('connection', newConnection);
 
 var players = []; //all socket ids
+var broadcasted = {};
 
 function newConnection(socket) {
-    //  console.log("new connection! " + socket.id);
 
     players.push(socket.id);
     console.log(players)
 
-    socket.on('turn', function (roundinfo) {
-        currPlayer = players[roundinfo.newIndex];
+    socket.on('turn', function (roundinfo) { //recieves increased index values from player that clicked next
+        playerIndex = roundinfo.newIndex //reassigns them
         currPhrase = roundinfo.newPhrase;
-        socket.to(currPlayer).emit('turn', currPhrase)
+        broadcasted = { //puts reassigned values into array
+            pindex: playerIndex,
+            pphrase: currPhrase
+        }
+
+        currPlayer = players[playerIndex];
+
+        socket.to(currPlayer).emit('turn', broadcasted); //sends those values to next player
     })
 
+    socket.on('update', function (roundinfo) { //recieves increased index values from player that clicked next
+        console.log(roundinfo);
+        socket.broadcast.emit('update', roundinfo); //sends those values back to everyone
+    })
+
+    socket.on('clear', clearMsg);
+    function clearMsg() {
+        socket.broadcast.emit('clear');
+    }
     socket.on('drawing', drawingMsg);
     function drawingMsg(points) {
         socket.broadcast.emit('drawing', points);
